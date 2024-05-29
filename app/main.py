5151ce9a3aa4a7ea33044d76ad100b77fe01a080
Ag <agata.a.rajska@gmail.com>
2024-05-29 17:04:19 +0200

    ??????????

--- app/main.py
+++ app/main.py
@@ -16,7 +16,7 @@ def main():
         elif command.split(" ")[0] == "type":
             cmd = command.split("type")[1]
             if cmd in Commands:
-                print("{cmd} is a shell bulletin")
+                print(cmd + " is a shell bulletin")
             else:
                 print(f"{cmd}: command not found")
         else: 
