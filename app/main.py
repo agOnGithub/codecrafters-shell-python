94c6d47e798f7efa756a68bcd519a7f048e44ec1
Ag <agata.a.rajska@gmail.com>
2024-05-29 17:05:05 +0200

    adding stripping

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ def main():
             msg = command.split("echo")[1]
             print(f"{msg}")
         elif command.split(" ")[0] == "type":
-            cmd = command.split("type")[1]
+            cmd = command.split("type")[1].strip()
             if cmd in Commands:
                 print(cmd + " is a shell bulletin")
             else:
