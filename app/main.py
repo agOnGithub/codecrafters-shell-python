3da1b39f974d2970cfb065818b71d45c078a4465
Ag <agata.a.rajska@gmail.com>
2024-05-29 21:07:46 +0200

    executable file handling

--- app/main.py
+++ app/main.py
@@ -1,13 +1,22 @@
 import sys
+import os
+
+def does_it_exist(path, cmd):
+    for folder in path.split(":"):
+        if os.path.isfile(f"{folder}/{bin_name}"):
+            return folder
+    return None
 
 def main():
     Commands = {"exit", "echo", "type"}
+    path = os.getenv("PATH")
     while True:
         sys.stdout.write("$ ")
         sys.stdout.flush()
 
         # Wait for user input
         command = input()
+        #PATH="/usr/bin:/usr/local/bin" ./your_shell.sh
         if command == "exit 0":
             sys.exit()
         elif command.split(" ")[0] == "echo":
@@ -18,7 +27,11 @@ def main():
             if cmd in Commands:
                 print(f"{cmd} is a shell builtin")
             else:
-                print(f"{cmd} not found")
+                bin_folder = does_it_exist(path, cmd)
+                if bin_folder:
+                    print(f"{cmd} is {bin_folder}/{cmd}")
+                else: 
+                    print(f"{cmd}: not found")
         else: 
             print(f"{command}: command not found")
 
